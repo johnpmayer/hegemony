bb2fb67734d28ee7ac0e658eeb0f354898e6be90
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2012-03-13 15:14:29 -0400

    todo boundary scan, labeling for freq >2

--- demos/backed-geodesic/scripts/geodesic.js
+++ demos/backed-geodesic/scripts/geodesic.js
@@ -5,6 +5,14 @@ define(
   ["vector"],
   function(vector) {
     
+    function Node(lbl, vec, u, v) {
+      
+      this.label = lbl;
+      this.point = vec;
+      this.instances = [{u:u,v:v}];
+      
+    }
+    
     // create the base icosahedron
     var icosahedron = {};
     
@@ -35,15 +43,25 @@ define(
       icosahedron.u_array[i+2][i] = south_pole;
     }
     
+    icosahedron.frequency = 1;
+    
+    icosahedron.boundaryScan = function() {
+      // ToDo
+    }
+    
+    icosahedron.addLabels = function() {
+      // ToDo
+      this.uniqueVertices = -1;
+    }
+    
     var Geodesic = function() {
       
-      this.setFrequency = function(f) {
+      this.doubleFrequency = function() {
         
-        var ico = this.__proto__;
-        
+        var f = this.frequency * 2;
+        var u_array = [];
         var max_u = f * 6; // 0 <= u <= 6f
-        //var max_v = f * 5; // 0 <= v <= 5f
-        
+
         // allocate space for the new u_array
         var u_array = [];
         for (var i = 0; i <= max_u; i += 1) {
@@ -51,54 +69,63 @@ define(
         }
         
         // copy the major vertices from the icosahedron
-        for (var i = 0; i < ico.u_array.length; i += 1) {
-          var ico_v_array = ico.u_array[i];
-          var v_array = u_array[f*i];
-          for (var j = 0; j < ico_v_array.length; j += 1) {
-            if (ico_v_array[j]) {            
-              v_array[f*j] = ico_v_array[j];
+        for (var i = 0; i < this.u_array.length; i += 1) {
+          var old_v_array = this.u_array[i];
+          var v_array = u_array[2*i];
+          for (var j = 0; j < old_v_array.length; j += 1) {
+            if (old_v_array[j]) {            
+              v_array[2*j] = old_v_array[j];
             }
           }
         }
         
         // interpolate along v_arrays
-        // for every v_array (the filled-in ones)
-        for (var i = 0; i < u_array.length; i += f) {
+        // for every v_array
+        for (var i = 0; i < u_array.length; i += 2) {
           var v_array = u_array[i];
-          // for every pair of vertical adjacent vertices (the
-          // filled-in ones)
-          for (var j = 0; j + f < v_array.length; j += f) {
-            if (v_array[j] && v_array[j+f]) {
-              // calculate the f-1 new vertices
+          // for every pair of vertical adjacent vertices
+          for (var j = 0; j + 2 < v_array.length; j += 2) {
+            if (v_array[j] && v_array[j+2]) {
+              // calculate the 1 new vertex
               var start = v_array[j];
-              var end = v_array[j+f];
-              var stepV = end.sub(start).scale(1/f);
-              for (var k = 1; k < f; k +=1) {
-                v_array[j+k] = start.add(stepV.scale(k));
-              }
+              var end = v_array[j+2];
+              var stepV = end.sub(start).scale(1/2);
+              v_array[j+1] = start.add(stepV);
             }
           }
         }
         
         // interpolate between v_arrays
-        // for every pair of v_arrays (the filled-in ones)
-        for (var i = 0; i + f < u_array.length; i += f) {
+        // for every pair of v_arrays
+        for (var i = 0; i + 2 < u_array.length; i += 2) {
           var v_array_a = u_array[i];
-          var v_array_b = u_array[i+f];
-          // for every two horizontal adjacent vertices (the ones from
-          // the icosahedron only)
-          for (var j = 0; j + f < v_array.length; j += f) {
+          var v_array_b = u_array[i+2];
+          for (var j = 0; j < v_array_a.length; j += 2) {
+            // for every two horizontal adjacent vertices
             if(v_array_a[j] && v_array_b[j]) {
               // calculate the f-1 new vertices horizontally
               var start = v_array_a[j];
               var end = v_array_b[j];
-              var stepV = end.sub(start).scale(1/f);
-              for (var k = 1; k < f; k += 1) {
-                u_array[i+k][j] = start.add(stepV.scale(k));
+              var stepV = end.sub(start).scale(1/2);
+              u_array[i+1][j] = start.add(stepV);
+            }
+            // for every two diagonally adjacent vertices
+            if(v_array_a[j] && v_array_b[j+2]) {
+              // calculate the f-1 new vertices diagonally
+              var start = v_array_a[j];
+              var end = v_array_b[j+2];
+              if (!(start === end)) {
+                var stepV = end.sub(start).scale(1/2);
+                u_array[i+1][j+1] = start.add(stepV);
               }
             }
           }
         }
+        
+        // boundary scan
+        
+        
+        
         
         this.frequency = f;
         this.u_array = u_array;
--- demos/backed-geodesic/scripts/main.js
+++ demos/backed-geodesic/scripts/main.js
@@ -10,16 +10,38 @@ require(["geodesic"], function(geodesic){
   write("geo = " + JSON.stringify(geo));
   write("geo.__proto__ = " + JSON.stringify(geo.__proto__));
   
-  geo.setFrequency(2);
+  geo.doubleFrequency();
   
   write("geo f=2:")
+  var u_count = 0;
   for (var i = 0; i < geo.u_array.length; i += 1) {
     var v_array = geo.u_array[i];
-    var count = 0;
+    var v_count = 0;
     for (var j = 0; j < v_array.length; j += 1) {
-      if (v_array[j]) count += 1;
+      if (v_array[j]) {
+        u_count += 1;
+        v_count += 1;
+      }
     }
-    write(i + " " + v_array.length + " " + count + " " + JSON.stringify(v_array));
+    write(i + " " + v_array.length + " " + v_count + " " + JSON.stringify(v_array));
   }
+  write(u_count);
+  
+  geo.doubleFrequency();
+  
+  write("geo f=4:")
+  var u_count = 0;
+  for (var i = 0; i < geo.u_array.length; i += 1) {
+    var v_array = geo.u_array[i];
+    var v_count = 0;
+    for (var j = 0; j < v_array.length; j += 1) {
+      if (v_array[j]) {
+        u_count += 1;
+        v_count += 1;
+      }
+    }
+    write(i + " " + v_array.length + " " + v_count + " " + JSON.stringify(v_array));
+  }
+  write(u_count);
   
 });
